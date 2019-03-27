3260cfbadf91b6f523f027bf4b1aaf9bb5f929c3
JaySebagh <jaysebagh@gmail.com>
2019-03-27 13:20:40 -0700

    cohorts migration

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const server = express();
 server.use(express.json());
 
 server.get('/', (req, res) => {
-    res.send('Hello')
+    res.send('Beep Boop, server is alive.')
 })
 
 module.exports = server;
